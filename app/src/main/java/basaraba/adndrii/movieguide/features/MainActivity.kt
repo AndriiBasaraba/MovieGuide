f16117041039c000b06fa44a977cb1ca57ea0ac8
AndriiBasaraba <apollon.trnpl@gmail.com>
2023-06-21 19:26:25 +0100

    added compose for project

--- app/src/main/java/basaraba/adndrii/movieguide/features/MainActivity.kt
+++ app/src/main/java/basaraba/adndrii/movieguide/features/MainActivity.kt
@@ -1,22 +1,42 @@
 package basaraba.adndrii.movieguide.features
 
+import MovieGuideTheme
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import basaraba.adndrii.movieguide.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding: ActivityMainBinding get() = checkNotNull(_binding)
-
-    private val movieDetailsViewModel: NowPlayingMoviesViewModel by viewModel()
+    private val viewModel: NowPlayingMoviesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            MovieGuideTheme {
+                Surface(
+                    modifier = Modifier.wrapContentWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Start()
+                }
+            }
+        }
+    }
+}
 
-        binding.btnReload.setOnClickListener { movieDetailsViewModel.getNowPlayingMoviesUseCase() }
+@Composable
+fun Start() {
+    Button(
+        onClick = { },
+    ) {
+        Text(text = "Load data from somewhere")
     }
 }
