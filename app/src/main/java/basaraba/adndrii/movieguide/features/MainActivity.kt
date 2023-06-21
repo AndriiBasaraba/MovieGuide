5d6a42fb1f2a4240fd2c8b75a95e33aeba00e03d
AndriiBasaraba <apollon.trnpl@gmail.com>
2023-06-21 20:18:49 +0100

    added koin support for compose

--- app/src/main/java/basaraba/adndrii/movieguide/features/MainActivity.kt
+++ app/src/main/java/basaraba/adndrii/movieguide/features/MainActivity.kt
@@ -4,38 +4,31 @@ import MovieGuideTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import androidx.compose.ui.unit.dp
+import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
-
-    private val viewModel: NowPlayingMoviesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieGuideTheme {
-                Surface(
-                    modifier = Modifier.wrapContentWidth(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Start()
-                }
+                Start(viewModel = koinViewModel())
             }
         }
     }
 }
 
 @Composable
-fun Start() {
+fun Start(viewModel: NowPlayingMoviesViewModel) {
     Button(
-        onClick = { },
+        onClick = { viewModel.getNowPlayingMoviesUseCase() },
+        modifier = Modifier.padding(16.dp)
     ) {
         Text(text = "Load data from somewhere")
     }
